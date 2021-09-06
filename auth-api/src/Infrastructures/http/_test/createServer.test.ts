dab40693ba2a14ffb41422e823e1ea6bc573e792
Agis <agis.septiyanto@gmail.com>
2021-09-07 06:31:10 +0700

    crate server test

---
+++ auth-api/src/Infrastructures/http/_test/createServer.test.ts
@@ -0,0 +1,37 @@
+import pool from "../../database/postgres/pool";
+import UsersTableTestHelper from "../../../../tests/UsersTableTestHelper";
+import container from "../../container";
+import createServer from "../createServer";
+
+describe('HTTP server',  () => {
+    afterAll(async () => {
+        await pool.end()
+    })
+
+    afterEach(async () => {
+        await UsersTableTestHelper.cleanTable()
+    })
+
+    describe('when POST /users',  ()=> {
+        it('should response 201 and persisted user', async () => {
+            const requestPayload = {
+                username: 'dicoding',
+                password: 'secret',
+                fullname: 'Dicoding Indonesia'
+            }
+            const server = await createServer(container)
+
+            const response = await server.inject({
+                method: 'POST',
+                url: '/users',
+                payload: requestPayload
+            })
+
+            const responseJson = JSON.parse(response.payload)
+            expect(response.statusCode).toEqual(201)
+            expect(responseJson.status).toEqual('success')
+            expect(responseJson.data.addedUser).toBeDefined()
+        });
+    })
+
+});
