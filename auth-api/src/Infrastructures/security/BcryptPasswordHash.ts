77bdb19f3fa576332312feebc7806b4547093b08
Agis <agis.septiyanto@gmail.com>
2021-09-06 12:41:50 +0700

    implement PasswordHash interface

---
+++ auth-api/src/Infrastructures/security/BcryptPasswordHash.ts
@@ -0,0 +1,17 @@
+import PasswordHash from "../../Applications/security/PasswordHash";
+
+class BcryptPasswordHash implements PasswordHash {
+    private bcrypt: any;
+    private readonly saltRound: number;
+
+    constructor(bcrypt: any, saltRound = 10) {
+        this.bcrypt = bcrypt;
+        this.saltRound = saltRound;
+    }
+
+    async hash(password: string): Promise<string> {
+        return this.bcrypt.hash(password, this.saltRound)
+    }
+}
+
+export default BcryptPasswordHash
