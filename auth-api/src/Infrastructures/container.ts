d3d3308848f25fcdd22c6ed61053ecd0b6b3f50c
agis <agista@dicoding.com>
2021-09-09 11:02:02 +0700

    format container

--- auth-api/src/Infrastructures/container.ts
+++ auth-api/src/Infrastructures/container.ts
@@ -1,66 +1,65 @@
-/*istanbul ignore file*/
-import {createContainer} from "instances-container";
-import {nanoid} from 'nanoid'
-import UserRepository from "../Domains/users/UserRepository";
-import UserRepositoryPostgres from "./repository/UserRepositoryPostgres";
-import pool from "./database/postgres/pool";
-import BcryptPasswordHash from "./security/BcryptPasswordHash";
+/* istanbul ignore file */
+import { createContainer } from 'instances-container'
+import { nanoid } from 'nanoid'
+import UserRepositoryPostgres from './repository/UserRepositoryPostgres'
+import pool from './database/postgres/pool'
+import BcryptPasswordHash from './security/BcryptPasswordHash'
 import bcrypt from 'bcrypt'
-import AddUserUseCase from "../Applications/use_case/AddUserUseCase";
-import {nameof} from "ts-simple-nameof";
-import Interfaces from "./Interfaces";
+import AddUserUseCase from '../Applications/use_case/AddUserUseCase'
+import { nameof } from 'ts-simple-nameof'
+import Interfaces from './Interfaces'
 
 const container = createContainer()
 
 // registering services and repository
 container.register([
-    {
-        key: nameof<Interfaces>(i => i.UserRepository),
-        Class: UserRepositoryPostgres,
-        parameter: {
-            dependencies: [
-                {
-                    concrete: pool
-                },
-                {
-                    concrete: nanoid
-                }
+  {
+    key: nameof<Interfaces>(i => i.UserRepository),
+    Class: UserRepositoryPostgres,
+    parameter: {
+      dependencies: [
+        {
+          concrete: pool
+        },
+        {
+          concrete: nanoid
+        }
 
-            ]
+      ]
+    }
+  },
+  {
+    key: nameof<Interfaces>(i => i.PasswordHash),
+    Class: BcryptPasswordHash,
+    parameter: {
+      dependencies: [
+        {
+          concrete: bcrypt
         }
-    },
-    {
-        key:  nameof<Interfaces>(i => i.PasswordHash),
-        Class: BcryptPasswordHash,
-        parameter: {
-            dependencies: [
-                {
-                    concrete: bcrypt,
-                }
-            ]
-        }
-    },
+      ]
+    }
+  }
 ])
 
 // registering use cases
 container.register([
-    {
-        key: AddUserUseCase.name,
-        Class: AddUserUseCase,
-        parameter: {
-            injectType: 'destructuring',
-            dependencies: [
-                {
-                    name: 'userRepository',
-                    internal: 'UserRepository'
-                },
-                {
-                    name: 'passwordHash',
-                    internal: 'PasswordHash'
-                },
-            ]
+  {
+    key: AddUserUseCase.name,
+    Class: AddUserUseCase,
+    parameter: {
+      injectType: 'destructuring',
+      dependencies: [
+        {
+          name: 'userRepository',
+          internal: 'UserRepository'
+        },
+        {
+          name: 'passwordHash',
+          internal: 'PasswordHash'
         }
+      ]
     }
+  }
 ])
 
 export default container
