790e46002350d2310f4069d78096cb6bc7bf1be9
agis <agista@dicoding.com>
2021-09-09 11:02:24 +0700

    add http test

--- auth-api/src/Interfaces/http/api/users/handler.ts
+++ auth-api/src/Interfaces/http/api/users/handler.ts
@@ -1,18 +1,22 @@
 import autoBind from 'auto-bind'
 import AddUserUseCase from '../../../../Applications/use_case/AddUserUseCase'
 import { Request, ResponseToolkit } from '@hapi/hapi'
+import DomainErrorTranslator from '../../../../Commons/exceptions/DomainErrorTranslator'
+import { Container } from 'instances-container'
+import ClientError from '../../../../Commons/exceptions/ClientError'
 
 class UsersHandler {
-    private container: any;
+  private container: Container;
 
-    constructor (container: any) {
-      this.container = container
+  constructor (container: any) {
+    this.container = container
 
-      autoBind(this)
-    }
+    autoBind(this)
+  }
 
-    async postUserHandler (request: Request, h: ResponseToolkit) {
-      const addUserUseCase = this.container.getInstance(AddUserUseCase.name)
+  async postUserHandler (request: Request, h: ResponseToolkit) {
+    try {
+      const addUserUseCase = this.container.getInstance(AddUserUseCase.name) as AddUserUseCase
       const addedUser = await addUserUseCase.execute(request.payload)
 
       const response = h.response({
@@ -23,7 +27,25 @@ class UsersHandler {
       })
       response.code(201)
       return response
+    } catch (error) {
+      const translatedError = DomainErrorTranslator.translate(error as Error)
+
+      if (translatedError instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: translatedError.message
+        })
+        response.code(translatedError.statusCode)
+        return response
+      }
+      const response = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami'
+      })
+      response.code(500)
+      return response
     }
+  }
 }
 
 export default UsersHandler
