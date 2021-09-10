df2fcebfc93bfc9b88033111a1a30633a35ae685
Agis <agis.septiyanto@gmail.com>
2021-09-10 09:05:58 +0700

    add function to UserRepository

--- auth-api/src/Domains/users/UserRepository.ts
+++ auth-api/src/Domains/users/UserRepository.ts
@@ -1,6 +1,8 @@
 import RegisteredUser from './entities/RegisteredUser/RegisteredUser'
 
 interface UserRepository {
+    getIdByUsername(username: string): Promise<string>
+    getPasswordByUsername(username: string): Promise<string>
     addUser(registerUser: any) : Promise<RegisteredUser>
     isUsernameUsed(username: string) : Promise<Boolean>
 }
