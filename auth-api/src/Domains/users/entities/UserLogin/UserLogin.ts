700407168cdc4bc1fba85e9cca56f5f284808725
Agis <agis.septiyanto@gmail.com>
2021-09-10 08:08:23 +0700

    create UserLogin

---
+++ auth-api/src/Domains/users/entities/UserLogin/UserLogin.ts
@@ -0,0 +1,19 @@
+class UserLogin {
+    username: string;
+    password: string;
+
+    constructor({username, password}: any) {
+        if (!username || !password) {
+            throw new Error('USER_LOGIN.NOT_CONTAIN_NEEDED_PROPERTY');
+        }
+
+        if (typeof username !== 'string' || typeof password !== 'string') {
+            throw new Error('USER_LOGIN.NOT_MEET_DATA_TYPE_SPECIFICATION');
+        }
+
+        this.username = username;
+        this.password = password;
+    }
+}
+
+export default UserLogin
