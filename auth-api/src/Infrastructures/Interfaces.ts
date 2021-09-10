838e3731fb9c61026a87567a50a1f13d64371159
Agis <agis.septiyanto@gmail.com>
2021-09-10 11:20:50 +0700

    register token manager and auth repo to interfaces

--- auth-api/src/Infrastructures/Interfaces.ts
+++ auth-api/src/Infrastructures/Interfaces.ts
@@ -1,7 +1,11 @@
 import UserRepository from '../Domains/users/UserRepository'
 import PasswordHash from '../Applications/security/PasswordHash'
+import AuthenticationRepository from "../Domains/authentications/AuthenticationRepository";
+import AuthenticationTokenManager from "../Applications/security/AuthenticationTokenManager";
 
 interface Interfaces{
+    AuthenticationTokenManager: AuthenticationTokenManager,
+    AuthenticationRepository: AuthenticationRepository,
     UserRepository: UserRepository,
     PasswordHash: PasswordHash
 }
