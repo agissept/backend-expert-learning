6983af98695c7c2f4165f12b09471d4778658c88
Agis <agis.septiyanto@gmail.com>
2021-09-10 12:31:32 +0700

    add interface to decode and verify token

--- auth-api/src/Applications/security/AuthenticationTokenManager.ts
+++ auth-api/src/Applications/security/AuthenticationTokenManager.ts
@@ -1,4 +1,8 @@
-interface AuthenticationTokenManager{
+interface AuthenticationTokenManager {
+    decodePayload(payload: any) : any
+
+    verifyRefreshToken(token: string): void
+
     createAccessToken(payload: { id: string; username: string }): void;
 
     createRefreshToken(p: { id: string; username: string }): void
