1b71bd210792250d041e1cb4920f4475679dd122
Agis <agis.septiyanto@gmail.com>
2021-09-10 09:12:34 +0700

    add create LoginUserUseCase to container

--- auth-api/src/Infrastructures/container.ts
+++ auth-api/src/Infrastructures/container.ts
@@ -1,65 +1,91 @@
 /* istanbul ignore file */
-import { createContainer } from 'instances-container'
-import { nanoid } from 'nanoid'
+import {createContainer} from 'instances-container'
+import {nanoid} from 'nanoid'
 import UserRepositoryPostgres from './repository/UserRepositoryPostgres'
 import pool from './database/postgres/pool'
 import BcryptPasswordHash from './security/BcryptPasswordHash'
 import bcrypt from 'bcrypt'
 import AddUserUseCase from '../Applications/use_case/AddUserUseCase'
-import { nameof } from 'ts-simple-nameof'
+import {nameof} from 'ts-simple-nameof'
 import Interfaces from './Interfaces'
+import LoginUserUseCase from "../Applications/use_case/LoginUserUseCase";
 
 const container = createContainer()
 
 // registering services and repository
 container.register([
-  {
-    key: nameof<Interfaces>(i => i.UserRepository),
-    Class: UserRepositoryPostgres,
-    parameter: {
-      dependencies: [
-        {
-          concrete: pool
-        },
-        {
-          concrete: nanoid
-        }
+    {
+        key: nameof<Interfaces>(i => i.UserRepository),
+        Class: UserRepositoryPostgres,
+        parameter: {
+            dependencies: [
+                {
+                    concrete: pool
+                },
+                {
+                    concrete: nanoid
+                }
 
-      ]
-    }
-  },
-  {
-    key: nameof<Interfaces>(i => i.PasswordHash),
-    Class: BcryptPasswordHash,
-    parameter: {
-      dependencies: [
-        {
-          concrete: bcrypt
+            ]
         }
-      ]
+    },
+    {
+        key: nameof<Interfaces>(i => i.PasswordHash),
+        Class: BcryptPasswordHash,
+        parameter: {
+            dependencies: [
+                {
+                    concrete: bcrypt
+                }
+            ]
+        }
     }
-  }
 ])
 
 // registering use cases
 container.register([
-  {
-    key: AddUserUseCase.name,
-    Class: AddUserUseCase,
-    parameter: {
-      injectType: 'destructuring',
-      dependencies: [
-        {
-          name: 'userRepository',
-          internal: 'UserRepository'
-        },
-        {
-          name: 'passwordHash',
-          internal: 'PasswordHash'
+    {
+        key: AddUserUseCase.name,
+        Class: AddUserUseCase,
+        parameter: {
+            injectType: 'destructuring',
+            dependencies: [
+                {
+                    name: 'userRepository',
+                    internal: 'UserRepository'
+                },
+                {
+                    name: 'passwordHash',
+                    internal: 'PasswordHash'
+                }
+            ]
         }
-      ]
+    },
+    {
+        key: LoginUserUseCase.name,
+        Class: LoginUserUseCase,
+        parameter: {
+            injectType: "destructuring",
+            dependencies: [
+                {
+                    name: 'userRepository',
+                    internal: 'UserRepository',
+                },
+                {
+                    name: 'authenticationRepository',
+                    internal: 'AuthenticationRepository',
+                },
+                {
+                    name: 'authenticationTokenManager',
+                    internal: 'AuthenticationTokenManager',
+                },
+                {
+                    name: 'passwordHash',
+                    internal: 'PasswordHash',
+                },
+            ]
+        }
     }
-  }
 ])
 
 export default container
