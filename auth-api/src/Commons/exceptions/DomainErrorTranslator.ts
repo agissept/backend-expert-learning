6ac2a2bb03bd572169d88b3f7b5151dccf5463af
Agis <agis.septiyanto@gmail.com>
2021-09-10 11:20:08 +0700

    add error translator

--- auth-api/src/Commons/exceptions/DomainErrorTranslator.ts
+++ auth-api/src/Commons/exceptions/DomainErrorTranslator.ts
@@ -6,7 +6,9 @@ class DomainErrorTranslator {
     'REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat membuat user baru karena tipe data tidak sesuai'),
     'REGISTER_USER.USERNAME_LIMIT_CHAR': new InvariantError('tidak dapat membuat user baru karena karakter username melebihi batas limit'),
     'REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER': new InvariantError('tidak dapat membuat user baru karena username mengandung karakter terlarang'),
-    'REGISTER_USER.USERNAME_IS_TAKEN': new InvariantError('username tidak tersedia')
+    'REGISTER_USER.USERNAME_IS_TAKEN': new InvariantError('username tidak tersedia'),
+    'USER_LOGIN.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('harus mengirimkan username dan password'),
+    'USER_LOGIN.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('username dan password harus string'),
   }
 
   static translate (error: Error) {
