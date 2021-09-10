26c129c504a622fcb5eb522c53b4821cac9694a7
Agis <agis.septiyanto@gmail.com>
2021-09-10 12:30:52 +0700

    create test to check flow of use case is correct

--- auth-api/src/Applications/use_case/_test/RefreshAuthenticationUseCase.test.ts
+++ auth-api/src/Applications/use_case/_test/RefreshAuthenticationUseCase.test.ts
@@ -1,4 +1,6 @@
 import RefreshAuthenticationUseCase from "../RefreshAuthenticationUseCase";
+import AuthenticationRepository from "../../../Domains/authentications/AuthenticationRepository";
+import AuthenticationTokenManager from "../../security/AuthenticationTokenManager";
 
 
 describe('RefreshAuthenticationUseCase', () => {
@@ -20,5 +22,37 @@ describe('RefreshAuthenticationUseCase', () => {
         await expect(refreshAuthenticationUseCase.execute(useCasePayload))
             .rejects
             .toThrowError('REFRESH_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+    });
+
+    it('should orchestrating the refresh authentication action correctly', async () => {
+        const useCasePayload = {
+            refreshToken: 'some_refresh_token'
+        }
+
+        const mockAuthenticationRepository = <AuthenticationRepository>{}
+        const mockAuthenticationTokenManager = <AuthenticationTokenManager>{}
+
+        mockAuthenticationRepository.checkAvailabilityToken = jest.fn()
+            .mockImplementation(() => Promise.resolve())
+        mockAuthenticationTokenManager.verifyRefreshToken = jest.fn()
+            .mockImplementation(() => Promise.resolve())
+        mockAuthenticationTokenManager.decodePayload = jest.fn()
+            .mockImplementation(() => Promise.resolve({username: 'dicoding', id: 'user-123'}))
+        mockAuthenticationTokenManager.createAccessToken = jest.fn()
+            .mockImplementation(() => Promise.resolve('some_new_access_token'))
+
+        const refreshAuthenticationUseCase = new RefreshAuthenticationUseCase({
+            authenticationRepository: mockAuthenticationRepository,
+            authenticationTokenManager: mockAuthenticationTokenManager
+        })
+
+        const accessToken = await refreshAuthenticationUseCase.execute(useCasePayload)
+
+        expect(mockAuthenticationTokenManager.verifyRefreshToken).toBeCalledWith(useCasePayload.refreshToken)
+        expect(mockAuthenticationRepository.checkAvailabilityToken).toBeCalledWith(useCasePayload.refreshToken)
+        expect(mockAuthenticationTokenManager.decodePayload).toBeCalledWith(useCasePayload.refreshToken)
+        expect(mockAuthenticationTokenManager.createAccessToken)
+            .toBeCalledWith({ username: 'dicoding', id: 'user-123' })
+        expect(accessToken).toEqual('some_new_access_token')
     });
 })
