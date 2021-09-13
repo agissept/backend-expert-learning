768d85bba47c0715969bb0ad9d41b6cabce01ebe
Agis <agis.septiyanto@gmail.com>
2021-09-13 08:20:45 +0700

    add query to add token

--- auth-api/src/Infrastructures/repository/AuthenticationRepositoryPostgres.ts
+++ auth-api/src/Infrastructures/repository/AuthenticationRepositoryPostgres.ts
@@ -9,7 +9,16 @@ class AuthenticationRepositoryPostgres implements AuthenticationRepository {
     this.pool = pool
   }
 
-  addToken (token: string): void {
+  async addToken (token: string): Promise<void> {
+    const query = {
+      text: 'INSERT INTO authentications VALUES($1) RETURNING token',
+      values: [token]
+    }
+    const result = await this.pool.query(query)
+
+    if (!result.rows) {
+      throw new Error('token gagal ditambahkan')
+    }
   }
 
   async checkAvailabilityToken (token: string): Promise<void> {
