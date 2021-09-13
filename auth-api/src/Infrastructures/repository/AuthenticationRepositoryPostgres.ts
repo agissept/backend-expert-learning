846e61b3b9eea78ebccf540369019c599857d28f
agis <agista@dicoding.com>
2021-09-13 07:54:47 +0700

    implement check availability token

--- auth-api/src/Infrastructures/repository/AuthenticationRepositoryPostgres.ts
+++ auth-api/src/Infrastructures/repository/AuthenticationRepositoryPostgres.ts
@@ -1,11 +1,27 @@
 import AuthenticationRepository from '../../Domains/authentications/AuthenticationRepository'
+import { Pool } from 'pg'
+import InvariantError from '../../Commons/exceptions/InvariantError'
 
 class AuthenticationRepositoryPostgres implements AuthenticationRepository {
+  private pool: Pool
+
+  constructor (pool: Pool) {
+    this.pool = pool
+  }
+
   addToken (token: string): void {
   }
 
-  checkAvailabilityToken (token: string): boolean {
-    return false
+  async checkAvailabilityToken (token: string): Promise<void> {
+    const query = {
+      text: 'SELECT * FROM authentications WHERE token = $1',
+      values: [token]
+    }
+    const result = await this.pool.query(query)
+
+    if (result.rows.length === 0) {
+      throw new InvariantError('refresh token tidak ditemukan di database')
+    }
   }
 
   deleteToken (token: string): void {
