ad058f1f447d1cc30343a3d0ba95f8e36c2c8383
Agis <agis.septiyanto@gmail.com>
2021-09-14 15:43:38 +0700

    change type data to any

--- auth-api/src/Commons/interface/UseCaseConstructor.ts
+++ auth-api/src/Commons/interface/UseCaseConstructor.ts
@@ -1,12 +1,2 @@
-import AuthenticationRepository from '../../Domains/authentications/AuthenticationRepository'
-import AuthenticationTokenManager from '../../Applications/security/AuthenticationTokenManager'
-import PasswordHash from '../../Applications/security/PasswordHash'
-import UserRepository from '../../Domains/users/UserRepository'
-
-interface UseCaseConstructor {
-    authenticationRepository: AuthenticationRepository
-    authenticationTokenManager: AuthenticationTokenManager
-    passwordHash: PasswordHash
-    userRepository: UserRepository
-}
+type UseCaseConstructor = any
 export default UseCaseConstructor
