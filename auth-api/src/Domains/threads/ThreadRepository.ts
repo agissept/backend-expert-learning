cda6dba39b885c09cb7f002b13f200b187a14349
Agis <agis.septiyanto@gmail.com>
2021-09-14 14:51:05 +0700

    change return type to AddedThread

--- auth-api/src/Domains/threads/ThreadRepository.ts
+++ auth-api/src/Domains/threads/ThreadRepository.ts
@@ -1,6 +1,7 @@
 import NewThread from './entities/NewThread'
+import AddedThread from './entities/AddedThread'
 
 interface ThreadRepository{
-    addThread(thread: NewThread): Promise<void>
+    addThread(thread: NewThread): Promise<AddedThread>
 }
 export default ThreadRepository
