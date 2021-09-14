e3fbe3fc5a4e25eb6f1421fae69e775189314264
Agis <agis.septiyanto@gmail.com>
2021-09-14 15:17:24 +0700

    create test when user put bad payload in body

--- auth-api/src/Infrastructures/http/_test/threads.test.ts
+++ auth-api/src/Infrastructures/http/_test/threads.test.ts
@@ -3,11 +3,16 @@ import container from '../../container'
 import LoginTestHelper from '../../../../tests/LoginTestHelper'
 import UsersTableTestHelper from '../../../../tests/UsersTableTestHelper'
 import AuthenticationsTableTestHelper from '../../../../tests/AuthenticationsTableTestHelper'
+import pool from '../../database/postgres/pool'
 
 describe('/threads endpoint', () => {
   afterEach(async () => {
     await UsersTableTestHelper.cleanTable()
     await AuthenticationsTableTestHelper.cleanTable()
+  })
+
+  afterAll(async () => {
+    await pool.end()
   })
 
   describe('when POST /threads', () => {
@@ -52,6 +57,29 @@ describe('/threads endpoint', () => {
       })
 
       expect(response.statusCode).toEqual(401)
+    })
+
+    it('should response 400 when payload is bad ', async () => {
+      const requestPayload = {
+        title: 'ini adalah judul'
+      }
+
+      const server = await createServer(container)
+      const userId = 'user-123'
+      const accessToken = await LoginTestHelper.getUserAccessToken(userId)
+
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+
+      const responseJson = JSON.parse(response.payload)
+      expect(response.statusCode).toEqual(400)
+      expect(responseJson.message).toEqual('tidak dapat membuat thread baru karena properti yang dibutuhkan tidak ada')
     })
   })
 })
