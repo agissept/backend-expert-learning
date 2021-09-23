074252992a6720ec3b53a576c4df72e2cbe9d4b3
agis <agis.septiyanto@gmail.com>
2021-09-23 14:15:36 +0700

    add getDetailThread in thread repo

--- auth-api/src/Domains/threads/ThreadRepository.ts
+++ auth-api/src/Domains/threads/ThreadRepository.ts
@@ -1,7 +1,10 @@
 import NewThread from './entities/NewThread'
 import AddedThread from './entities/AddedThread'
+import DetailThread from './model/DetailThread'
 
 interface ThreadRepository{
+    getDetailThread(threadId: string): Promise<DetailThread>
+
     addThread(thread: NewThread): Promise<AddedThread>
 
     isThreadHasCreated(threadId: string): Promise<boolean>
