da05fe8f6377b5f350e65dde95a35cceec04a6f6
agis <agis.septiyanto@gmail.com>
2021-09-26 07:55:42 +0700

    add comment repository

--- auth-api/src/Applications/use_case/Thread/GetDetailThreadUseCase.ts
+++ auth-api/src/Applications/use_case/Thread/GetDetailThreadUseCase.ts
@@ -1,16 +1,19 @@
 import UnvalidatedPayload from '../../../Commons/interface/UnvalidatedPayload'
 import ThreadRepository from '../../../Domains/threads/ThreadRepository'
 import GetDetailThreadFactory from '../../../Domains/threads/entities/GetDetailThread/GetDetailThreadFactory'
+import CommentRepository from '../../../Domains/comment/CommentRepository'
 
 class GetDetailThreadUseCase {
   private readonly threadRepository: ThreadRepository;
+  private readonly commentRepository: CommentRepository;
 
-  constructor ({ threadRepository }: UnvalidatedPayload) {
+  constructor ({ threadRepository, commentRepository }: UnvalidatedPayload) {
     this.threadRepository = threadRepository
+    this.commentRepository = commentRepository
   }
 
   async execute (threadId: string) {
-    const factory = new GetDetailThreadFactory(this.threadRepository)
+    const factory = new GetDetailThreadFactory(this.threadRepository, this.commentRepository)
     return await factory.create(threadId)
   }
 }
