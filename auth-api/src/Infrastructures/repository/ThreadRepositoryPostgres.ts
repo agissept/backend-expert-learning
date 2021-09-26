9400b235125b31deb5c2c85126363baf90abfee5
agis <agis.septiyanto@gmail.com>
2021-09-26 07:56:33 +0700

    add getDetailThread implementation

--- auth-api/src/Infrastructures/repository/ThreadRepositoryPostgres.ts
+++ auth-api/src/Infrastructures/repository/ThreadRepositoryPostgres.ts
@@ -3,13 +3,39 @@ import NewThread from '../../Domains/threads/entities/NewThread'
 import { Pool } from 'pg'
 import IdGenerator from '../util/IdGenerator/IdGenerator'
 import AddedThread from '../../Domains/threads/entities/AddedThread'
+import ThreadDTO from '../../Domains/threads/model/RepositoryModel/ThreadDTO'
 
 class ThreadRepositoryPostgres implements ThreadRepository {
     private pool: Pool
     private idGenerator: IdGenerator
+
     constructor (pool: Pool, idGenerator: IdGenerator) {
       this.pool = pool
       this.idGenerator = idGenerator
+    }
+
+    async getDetailThread (threadId: string): Promise<ThreadDTO> {
+      const query = {
+        text: `SELECT threads.*, users.username
+               FROM threads
+               JOIN users ON users.id = threads.user_id
+               WHERE threads.id = $1`,
+        values: [threadId]
+      }
+      const { rows, rowCount } = await this.pool.query(query)
+      const thread = rows[0]
+
+      if (!rowCount) {
+        return undefined as unknown as ThreadDTO
+      }
+
+      return {
+        body: thread.body,
+        date: thread.created_at,
+        id: thread.id,
+        title: thread.title,
+        username: thread.username
+      }
     }
 
     async addThread (newThread: NewThread): Promise<AddedThread> {
