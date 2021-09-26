4469816aa38ecb7117747facb3ddc1dffbf314cc
agis <agis.septiyanto@gmail.com>
2021-09-26 07:58:51 +0700

    implement handler

--- auth-api/src/Interfaces/http/api/threads/handler.ts
+++ auth-api/src/Interfaces/http/api/threads/handler.ts
@@ -2,6 +2,7 @@ import { Container } from 'instances-container'
 import { Request, ResponseToolkit } from '@hapi/hapi'
 import autoBind from 'auto-bind'
 import AddThreadUseCase from '../../../../Applications/use_case/Thread/AddThreadUseCase'
+import GetDetailThreadUseCase from '../../../../Applications/use_case/Thread/GetDetailThreadUseCase'
 
 class ThreadsHandler {
     private readonly container: Container;
@@ -24,6 +25,16 @@ class ThreadsHandler {
       })
       response.code(201)
       return response
+    }
+
+    async getDetailThreadHandler ({ params }: Request) {
+      const { threadId } = params
+      const getDetailThreadUseCase = this.container.getInstance(GetDetailThreadUseCase.name) as GetDetailThreadUseCase
+      const thread = await getDetailThreadUseCase.execute(threadId)
+      return {
+        status: 'success',
+        data: { thread }
+      }
     }
 }
 
