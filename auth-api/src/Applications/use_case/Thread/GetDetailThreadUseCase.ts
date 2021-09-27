ea2bdcbb9a62abe91994413e9b7c1b86d62d4667
agis <agis.septiyanto@gmail.com>
2021-09-27 22:08:13 +0700

    fix detail thread usecase

--- auth-api/src/Applications/use_case/Thread/GetDetailThreadUseCase.ts
+++ auth-api/src/Applications/use_case/Thread/GetDetailThreadUseCase.ts
@@ -2,18 +2,21 @@ import UnvalidatedPayload from '../../../Commons/interface/UnvalidatedPayload'
 import ThreadRepository from '../../../Domains/threads/ThreadRepository'
 import GetDetailThreadFactory from '../../../Domains/threads/factory/GetDetailThread/GetDetailThreadFactory'
 import CommentRepository from '../../../Domains/comment/CommentRepository'
+import ReplyRepository from '../../../Domains/replies/ReplyRepository'
 
 class GetDetailThreadUseCase {
   private readonly threadRepository: ThreadRepository;
   private readonly commentRepository: CommentRepository;
+  private readonly replyRepository: ReplyRepository;
 
-  constructor ({ threadRepository, commentRepository }: UnvalidatedPayload) {
+  constructor ({ threadRepository, commentRepository, replyRepository }: UnvalidatedPayload) {
     this.threadRepository = threadRepository
     this.commentRepository = commentRepository
+    this.replyRepository = replyRepository
   }
 
   async execute (threadId: string) {
-    const factory = new GetDetailThreadFactory(this.threadRepository, this.commentRepository)
+    const factory = new GetDetailThreadFactory(this.threadRepository, this.commentRepository, this.replyRepository)
     return await factory.create(threadId)
   }
 }
--- auth-api/src/Applications/use_case/Thread/_test/GetDetailThreadUseCase.test.ts
+++ auth-api/src/Applications/use_case/Thread/_test/GetDetailThreadUseCase.test.ts
@@ -2,68 +2,136 @@ import ThreadDTO from '../../../../Domains/threads/model/RepositoryModel/ThreadD
 import CommentDTO from '../../../../Domains/comment/model/RepositoryModel/CommentDTO'
 import GetDetailThreadUseCase from '../GetDetailThreadUseCase'
 import ThreadRepository from '../../../../Domains/threads/ThreadRepository'
-import Comment from '../../../../Domains/comment/model/DomainModel/Comment'
 import ThreadWithComments from '../../../../Domains/threads/model/DomainModel/ThreadWithComments'
 import CommentRepository from '../../../../Domains/comment/CommentRepository'
+import ReplyRepository from '../../../../Domains/replies/ReplyRepository'
+import ReplyDTO from '../../../../Domains/replies/model/ReplyDTO'
 
 describe('GetDetailThreadUseCase', () => {
   it('should orchestrating the get detail thread action correctly ', async () => {
     const threadId = 'thread-123'
 
-    const firstComment: CommentDTO = {
-      id: 'comment-123',
-      username: 'john',
-      date: '2021-08-08T07:22:33.555Z',
-      content: 'sebuah komentar',
-      isDeleted: false,
-      userId: 'john-123'
-    }
-
-    const secondComment: CommentDTO = {
-      id: 'comment-321',
-      username: 'asep',
-      date: '2021-09-08T07:22:33.555Z',
-      content: 'ini adalah komentar',
-      isDeleted: true,
-      userId: 'asep-123'
-    }
-
-    const detailThreadFromDB: ThreadDTO = {
+    const thread: ThreadDTO = {
       id: threadId,
       title: 'sebuah judul thread',
       body: 'sebuah isi thread',
-      date: '2021-08-08T07:19:09.775Z',
+      date: Date.now().toString(),
+      username: 'asep'
+    }
+
+    const comments: CommentDTO[] = [
+      {
+        id: 'comment-1',
+        username: 'agis',
+        date: Date.now().toString(),
+        content: 'ini komentar pertama',
+        isDeleted: false,
+        userId: 'user-1'
+      },
+      {
+        id: 'comment-2',
+        username: 'asep',
+        date: Date.now().toString(),
+        content: 'ini komentar kedua',
+        isDeleted: true,
+        userId: 'user-2'
+      }
+    ]
+
+    const replies: ReplyDTO[] = [{
+      id: 'reply-1',
+      userId: 'user-1',
+      commentId: 'comment-1',
+      content: 'balasan dari agis ke komentar 1',
+      isDeleted: true,
+      createdAt: Date.now().toString(),
       username: 'agis'
-    }
-
-    const expectedFirstComment: Comment = {
-      id: 'comment-123',
-      username: 'john',
-      date: '2021-08-08T07:22:33.555Z',
-      content: 'sebuah komentar'
-    }
-
-    const expectedSecondComment: Comment = {
-      id: 'comment-321',
-      username: 'asep',
-      date: '2021-09-08T07:22:33.555Z',
-      content: '**komentar telah dihapus**'
-    }
+    },
+    {
+      id: 'reply-2',
+      userId: 'user-2',
+      commentId: 'comment-1',
+      content: 'balasan dari asep ke komentar 1',
+      isDeleted: false,
+      createdAt: Date.now().toString(),
+      username: 'asep'
+    },
+    {
+      id: 'reply-3',
+      userId: 'user-1',
+      commentId: 'comment-2',
+      content: 'balasan dari agis ke komentar 2',
+      isDeleted: false,
+      createdAt: Date.now().toString(),
+      username: 'agis'
+    },
+    {
+      id: 'reply-4',
+      userId: 'user-2',
+      commentId: 'comment-2',
+      content: 'balasan dari asep ke komentar 2',
+      isDeleted: false,
+      createdAt: Date.now().toString(),
+      username: 'asep'
+    }]
 
     const expectedThread: ThreadWithComments = {
-      id: threadId,
-      title: 'sebuah judul thread',
-      body: 'sebuah isi thread',
-      date: '2021-08-08T07:19:09.775Z',
-      username: 'agis',
-      comments: [expectedFirstComment, expectedSecondComment]
+      id: thread.id,
+      title: thread.title,
+      body: thread.body,
+      date: thread.date,
+      username: thread.username,
+      comments: [
+        {
+          id: comments[0].id,
+          username: comments[0].username,
+          date: comments[0].date,
+          content: comments[0].content,
+          replies: [
+            {
+              id: replies[0].id,
+              username: replies[0].username,
+              content: '**balasan telah dihapus**',
+              date: replies[0].createdAt
+            },
+            {
+              id: replies[1].id,
+              username: replies[1].username,
+              content: replies[1].content,
+              date: replies[1].createdAt
+            }
+          ]
+        },
+        {
+          id: comments[1].id,
+          username: comments[1].username,
+          date: comments[1].date,
+          content: '**komentar telah dihapus**',
+          replies: [
+            {
+              id: replies[2].id,
+              username: replies[2].username,
+              content: replies[2].content,
+              date: replies[2].createdAt
+            },
+            {
+              id: replies[3].id,
+              username: replies[3].username,
+              content: replies[3].content,
+              date: replies[3].createdAt
+            }
+          ]
+        }
+      ]
     }
 
     const threadRepository = <ThreadRepository>{}
     const commentRepository = <CommentRepository>{}
-    const getDetailThreadUseCase = new GetDetailThreadUseCase({ threadRepository, commentRepository })
-    threadRepository.getDetailThread = jest.fn(() => Promise.resolve(detailThreadFromDB))
-    commentRepository.getCommentsByThreadId = jest.fn(() => Promise.resolve([firstComment, secondComment]))
+    const replyRepository = <ReplyRepository>{}
+    const getDetailThreadUseCase = new GetDetailThreadUseCase({ threadRepository, commentRepository, replyRepository })
+    threadRepository.getDetailThread = jest.fn(() => Promise.resolve(thread))
+    commentRepository.getCommentsByThreadId = jest.fn(() => Promise.resolve(comments))
+    replyRepository.getRepliesByCommentIds = jest.fn(() => Promise.resolve(replies))
     const detailThread = await getDetailThreadUseCase.execute(threadId)
 
     expect(expectedThread).toStrictEqual(detailThread)
