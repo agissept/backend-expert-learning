0f4bbe4c52fd0a678fe7fe9ab9a38e06b3e62414
agis <agis.septiyanto@gmail.com>
2021-09-28 09:25:03 +0700

    fix pool and .env.test

--- auth-api/src/Infrastructures/database/postgres/pool.ts
+++ auth-api/src/Infrastructures/database/postgres/pool.ts
@@ -1,5 +1,12 @@
 /* istanbul ignore file */
 import dotenv from 'dotenv-flow'
 import { Pool } from 'pg'
-dotenv.config({ silent: true })
-export default new Pool()
+const { parsed } = dotenv.config({ silent: true })
+const pool = new Pool({
+  database: parsed?.PGDATABASE,
+  user: parsed?.PGUSER,
+  password: parsed?.PGPASSWORD,
+  host: parsed?.PGHOST,
+  port: parsed?.PGPORT as unknown as number
+})
+export default pool
