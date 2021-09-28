f611a33170a981975b98e583cdbdfc3a09598ee7
agis <agis.septiyanto@gmail.com>
2021-09-28 07:51:31 +0700

    add soft delete reply interface

--- auth-api/src/Domains/replies/ReplyRepository.ts
+++ auth-api/src/Domains/replies/ReplyRepository.ts
@@ -5,6 +5,7 @@ interface ReplyRepository {
     addReply(newReply: NewReply): Promise<string>
     getRepliesByCommentIds(commentIds: string[]): Promise<ReplyDTO[]>
     getReplyById(replyId: string): Promise<ReplyDTO>;
+    softDeleteReply(replyId: string): Promise<void>
 }
 
 export default ReplyRepository
