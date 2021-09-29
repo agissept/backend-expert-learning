ea4b300a578c5041a4fb291e6bc4857c955254d5
agis <agis.septiyanto@gmail.com>
2021-09-29 12:47:17 +0700

    remove unused code

--- auth-api/src/Infrastructures/http/createServer.ts
+++ auth-api/src/Infrastructures/http/createServer.ts
@@ -59,14 +59,6 @@ const createServer = async (container: Container) => {
     }
   ])
 
-  await server.route({
-    method: 'GET',
-    path: '/',
-    handler: () => ({
-      value: 'Hello world!'
-    })
-  })
-
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request
@@ -92,7 +84,6 @@ const createServer = async (container: Container) => {
         status: 'error',
         message: 'terjadi kegagalan pada server kami'
       })
-      console.log(response)
       newResponse.code(500)
       return newResponse
     }
